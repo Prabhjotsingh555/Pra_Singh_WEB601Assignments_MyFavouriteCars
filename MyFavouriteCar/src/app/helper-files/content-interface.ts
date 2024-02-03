d444e431ec88aff89d71b57f2d1969f65d80d82c
Prabhjot singh <prabhjot.singh7131@icloud.com>
2024-02-02 20:16:26 -0500

    a3

--- MyFavouriteCar/src/app/helper-files/content-interface.ts
+++
@@ -1,9 +0,0 @@
-export interface Content {
-    id: number;
-    title: string;
-    description: string;
-    creator: string;
-    imgURL?: string; //optional
-    type?: string;
-    tags?: string[];
-  }
