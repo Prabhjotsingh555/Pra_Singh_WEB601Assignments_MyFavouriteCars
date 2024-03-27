be09c361b9fae1ffaccbc05e80aab44448638638
Prabhjotsingh555 <prabhjot.singh7131@icloud.com>
2024-03-27 16:26:38 -0400

    cc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,19 +7,26 @@ import { of } from 'rxjs';
 
 import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
-import {Content} from './helper-files/content-interface';
+import { Content } from './helper-files/content-interface';
 import { Sportservice } from './sports.service';
-import {MessagesComponent} from './messages/messages.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContentCardComponent, ContentListComponent, FormsModule, CommonModule, MessagesComponent],
+  imports: [
+    RouterOutlet,
+    ContentCardComponent,
+    ContentListComponent,
+    FormsModule,
+    CommonModule,
+    MessagesComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit{
-  title = 'Yashraj\'s Favorite Sports';
+export class AppComponent implements OnInit {
+  title = 'Prabhjot Favorite Cars';
 
   //declare property custom content of type 'Content' or undefined
   customContent: Content | undefined;
@@ -29,13 +36,14 @@ export class AppComponent implements OnInit{
 
   ngOnInit(): void {
     //choose ID
-    this.getContentById(1); 
+    this.getContentById(1);
   }
 
   //fetches content by id using movieservice
   getContentById(id: number): void {
-    //invokes method from MovieService, returns an Observable that outputs a movie Content based on its id 
-    this.movieService.getContentById(id)
+    //invokes method from MovieService, returns an Observable that outputs a movie Content based on its id
+    this.movieService
+      .getContentById(id)
       .pipe(
         //tap - performing side-effects (updates) without modifying the value
         //sets customContent to the fetched content based on id
@@ -44,12 +52,11 @@ export class AppComponent implements OnInit{
         }),
         catchError((error) => {
           console.error('Error fetching content by ID:', error);
-          //returns an observable null value 
-          return of(null); 
+          //returns an observable null value
+          return of(null);
         })
       )
       //initiates the observable chain and start retrieving the data
       .subscribe();
   }
 }
-
